7f5b7d4d7a31b4477d1469d43cd61d3f890cd68d
Dan Jakob Ofer <dan@ofer.to>
2026-06-20 09:31:07 -0400

    chore(bugs): code builds

--- solution.h
+++ solution.h
@@ -1,4 +1,9 @@
 typedef struct Tower Tower;
 
+#define TOWER_COUNT 3
+
 void
-solve_tower_of_hanoi(struct Tower*, unsigned char);
+create_towers(struct Tower** towers, unsigned char);
+
+void
+solve_tower_of_hanoi(struct Tower*);
