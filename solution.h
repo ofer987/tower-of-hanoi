793ed549347bae7b5a9fc88d3027e5ae57cf1b97
Dan Jakob Ofer <dan@ofer.to>
2026-07-19 11:57:59 -0400

    chore(towers): allocate in stack

--- solution.h
+++ solution.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <stddef.h>
 typedef struct Tower Tower;
 
 #define TOWER_COUNT 3
+
+struct Tower*
+get_tower_by_index(struct Tower*, size_t);
 
 unsigned char*
 get_tower_stacks(struct Tower*);
@@ -10,11 +14,11 @@ get_tower_stacks(struct Tower*);
 unsigned char
 get_current_tower_height(struct Tower*);
 
-struct Tower**
-create_towers(unsigned char);
+struct Tower*
+init_towers(unsigned char);
 
 void
-solve_tower_of_hanoi(struct Tower**);
+solve_tower_of_hanoi(struct Tower*);
 
 #ifdef TOWER_TEST_HOOKS
 // Only declared when TOWER_TEST_HOOKS is defined at compile time (see the
@@ -22,5 +26,5 @@ solve_tower_of_hanoi(struct Tower**);
 // observe the state of all three towers after every individual move made
 // while solving.
 void
-tower_test_on_move(struct Tower**);
+tower_test_on_move(struct Tower*);
 #endif
